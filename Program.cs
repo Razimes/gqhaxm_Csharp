4eb8d4f4eb51972b254f091f454c6436eb8598a1
Razimes <79305708+Razimes@users.noreply.github.com>
2022-09-15 09:19:55 +0200

    nyers koód

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gqhaxm_prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            szemetgyujto_demo myObj;
+            while (true)
+            {
+                myObj = new szemetgyujto_demo();
+                if(szemetgyujto_demo.adattag == 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ szemetgyujto_demo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gqhaxm_prog
+{
+    class szemetgyujto_demo 
+    {
+        public static int adattag = 1;
+        public szemetgyujto_demo()
+        {
+            adattag++;
+            Console.WriteLine(adattag);
+        }
+
+        ~szemetgyujto_demo()
+        {
+            adattag = 0;
+        }
+        
+       
+
+    }
+}
